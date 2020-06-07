027d2da5ce7566d10c6520bcc141801637dc54d8
Damien Meere <damien.meere@bt.com>
2020-06-07 13:32:14 +0000

    item object edit functionality in place

--- todo/forms.py
+++ todo/forms.py
@@ -5,4 +5,4 @@ from .models import Item
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        field = ['name', 'done']
+        fields = ['name', 'done']
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
+
 
 # Create your views here.
 def get_todo_list(request):
@@ -13,14 +14,42 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
+        # instead of pulling the posted variables outselves, we use the Django form to access them
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+        # name = request.POST.get('item_name')
+        # done = 'done' in request.POST
+        # Item.objects.create(name=name, done=done)
 
-        return redirect('get_todo_list')
     form = ItemForm()
+    # context contains empty form
     context = {
         'form': form
     }
-
     return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    # get_object_or_404 - used to say we want to get an instance of
+    # the item model, with an ID equal to the item ID that was
+    # passed into the view via the URL. or it will return 404 error
+    item = get_object_or_404(Item, id=item_id)
+    # POST handler
+    if request.method == 'POST':
+        # instead of pulling the posted variables outselves, we use the Django form to access them
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+
+    # create an instance of our item form and return it to the
+    # template in the context, pre-populated with the items
+    # current details.
+    form = ItemForm(instance=item)
+    # context contains empty form
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
